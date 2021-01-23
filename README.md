c7414a8871533f3b7b0b3deaa720336b3a4ad1aa
Carlo Munguia <carlomunguia@icloud.com>
2021-01-22 22:22:09 -0600

    created Csv reader, added soccer data to parse through, function to read file through node std lib package

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # wonder_stats
+
+# a WONDERFUL Typescript application to parse CSV data using Node.JS!
+
+# to build!:
+
+# cd into the source directory
+
+# run npm start to execute the application
---
+++ build/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const CsvFileReader_1 = require("./CsvFileReader");
+const reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+let manUnitedWins = 0;
+for (let match of reader.data) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
