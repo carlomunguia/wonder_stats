79a127bdddc33ab047bd03f2ad5283d1a6877216
Carlo Munguia <carlomunguia@icloud.com>
2021-01-24 13:57:02 -0600

    feature complete on Wonder Stats! we can now parse csv files and output to both console & html using typescript & nodegit add .!

--- build/Summary.js
+++ build/Summary.js
@@ -1,10 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Summary = void 0;
+const WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
+const HtmlReport_1 = require("./reportTargets/HtmlReport");
 class Summary {
     constructor(analyzer, outputTarget) {
         this.analyzer = analyzer;
         this.outputTarget = outputTarget;
+    }
+    static winsAnalysisWithHtmlReport(team) {
+        return new Summary(new WinsAnalysis_1.WinsAnalysis(team), new HtmlReport_1.HtmlReport());
     }
     buildAndPrintReport(matches) {
         const output = this.analyzer.run(matches);
--- build/index.js
+++ build/index.js
@@ -1,12 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./MatchReader");
-const CsvFileReader_1 = require("./CsvFileReader");
-const WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
 const Summary_1 = require("./Summary");
-const HtmlReport_1 = require("./reportTargets/HtmlReport");
-const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
-const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+const matchReader = MatchReader_1.MatchReader.fromCsv('football.csv');
+const summary = Summary_1.Summary.winsAnalysisWithHtmlReport('Everton');
 matchReader.load();
-const summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new HtmlReport_1.HtmlReport());
 summary.buildAndPrintReport(matchReader.matches);
